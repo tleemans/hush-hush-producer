fdf77f710097b051db04d2fece7efa980cd7d8de
Victor Alves <victor.alves@sentia.com>
2018-11-29 10:06:04 +0100

    Refactor current script to use threads and also add direction field

--- producer.py
+++ producer.py
@@ -1,83 +1,115 @@
-import pyaudio
-import wave
-import sox
-import requests
+import os
 import json
+import requests
+import threading
+import sox
+import wave
+import time
+import pyaudio
 
+from voice_engine.doa_respeaker_4mic_array import DOA
 from datetime import datetime
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+API_KEY = "<api-key-here>"
+API_URI = "<endpoint-here>"
 
-headers = {
-    'Content-type': 'application/json',
-    'x-api-key': '<api-key-here>' 
-}
+class Mic:
+    CHANNELS = 1
+    RATE = 16000
+    CHUNK = 1024
+    RECORD_SECONDS = 3
+    DEVICE_ID = "device-0002"
+    WAVE_OUTPUT_FILENAME = "output.wav"
+    FORMAT = pyaudio.paInt16
 
-print ("+---------------------------------+")
-print ("| Press Ctrl+C to Break Recording |")
-print ("+---------------------------------+")
 
-global keep_going
-keep_going = True
+    def __init__(self):
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
 
-while keep_going:
-    try:
+
+    def get_audio_data(self):
         audio = pyaudio.PyAudio()
+        stream = audio.open(format=self.FORMAT,
+                            channels=self.CHANNELS,
+                            rate=self.RATE,
+                            input=True,
+                            frames_per_buffer=self.CHUNK)
 
-        stream = audio.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
-
-        print("* recording")
         frames = []
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
+        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+            data = stream.read(self.CHUNK)
             frames.append(data)
-        print("* done recording")
 
         stream.stop_stream()
         stream.close()
         audio.terminate()
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(audio.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
+        return b''.join(frames), audio.get_sample_size(self.FORMAT)
+
+
+    def get_audio_stats(self, data, sample_size):
+        wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(sample_size)
+        wf.setframerate(self.RATE)
+        wf.writeframes(data)
         wf.close()
 
-        recording_data_dict = sox.file_info.stat(WAVE_OUTPUT_FILENAME)
+        recording_data_dict = sox.file_info.stat(self.WAVE_OUTPUT_FILENAME)
         recording_data = list(recording_data_dict.values())
         recording_date = datetime.now()
 
-        data = {}
-        data['DeviceId'] = "device-0002"
-        data["RecordingDate"] = recording_date.strftime("%Y%m%d%H%M%S")
-        data["MinimumAmplitude"] = str(recording_data[0])
-        data["RoughFrequency"] = str(recording_data[1])
-        data["ScaledBy"] = str(recording_data[2])
-        data["MeanNorm"] = str(recording_data[3])
-        data["MaximumDelta"] = str(recording_data[4])
-        data["RmsAmplitude"] = str(recording_data[5])
-        data["Length"] = str(recording_data[6])
-        data["SamplesRead"] = str(recording_data[7])
-        data["VolumeAdjustment"] = str(recording_data[8])
-        data["MinimumDelta"] = str(recording_data[9])
-        data["MidlineAmplitude"] = str(recording_data[10])
-        data["RmsDelta"] = str(recording_data[11])
-        data["MeanAmplitude"] = str(recording_data[12])
-        data["MeanDelta"] = str(recording_data[13])
-        data["MaximumAmplitude"] = str(recording_data[14])
+        doa = DOA(rate=self.RATE)
+        doa.put(data)
 
-        r = requests.post("<endpoint-here>", data=json.dumps(data), headers=headers)
-    except KeyboardInterrupt:
-        keep_going=False
-    except:
-        pass
+        return {
+            "MinimumAmplitude": str(recording_data[0]),
+            "RoughFrequency": str(recording_data[1]),
+            "ScaledBy": str(recording_data[2]),
+            "MeanNorm": str(recording_data[3]),
+            "MaximumDelta": str(recording_data[4]),
+            "RmsAmplitude": str(recording_data[5]),
+            "Length": str(recording_data[6]),
+            "SamplesRead": str(recording_data[7]),
+            "VolumeAdjustment": str(recording_data[8]),
+            "MinimumDelta": str(recording_data[9]),
+            "MidlineAmplitude": str(recording_data[10]),
+            "RmsDelta": str(recording_data[11]),
+            "MeanAmplitude": str(recording_data[12]),
+            "MeanDelta": str(recording_data[13]),
+            "MaximumAmplitude": str(recording_data[14]),
+            "DeviceId": self.DEVICE_ID,
+            "Direction": doa.get_direction(),
+            "RecordingDate": recording_date.strftime("%Y%m%d%H%M%S")
+        }
+
+
+    def push_stats(self, stats):
+        print("{}".format(stats))
+        # requests.post(API_URI, data=json.dumps(stats),
+        #     headers={"Content-type": "application/json", "x-api-key": API_KEY})
+
+
+    def clean(self):
+        os.remove(self.WAVE_OUTPUT_FILENAME)
+
+
+    def _run(self):
+        while True:
+            data, sample_size = self.get_audio_data()
+            stats = self.get_audio_stats(data, sample_size)
+            self.push_stats(stats)
+
+
+start_record = Mic()
+
+if __name__ == '__main__':
+    keep_running = True
+    while keep_running:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            keep_running = False
+            start_record.clean()
